792ff7f38b64105feab82d3d757492af5a65785b
Aksh Gupta <240108006@hbtu.ac.in>
2026-06-03 09:49:49 +0000

    container config done

---
+++ JavaScript/4-typeconversion.js
@@ -0,0 +1,54 @@
+// CONVERTING TO number
+let a= 123456789n
+let b = null
+let c;
+let d= true;
+let e= "aksh"
+let f= Number(a)
+let g= Number(b)
+let h= Number(c)
+let i= Number(d)
+let j= Number(e)
+console.log(f)
+console.log(g)
+console.log(h)
+console.log(i)
+console.log(j)
+
+console.log("")
+// CONVERTING TO boolean
+let a1= 123456789n
+let b1 = null
+let c1;
+let d1= 123;
+let e1= "aksh"
+let f1= Boolean(a1)
+let g1= Boolean(b1)
+let h1= Boolean(c1)
+let i1= Boolean(d1)
+let j1= Boolean(e1)
+console.log(f1)
+console.log(g1)
+console.log(h1)
+console.log(i1)
+console.log(j1)
+
+
+console.log("")
+// CONVERTING TO string
+let a2= 123456789n
+let b2 = null
+let c2;
+let d2= 123;
+let e2= "aksh"
+let f2= String(a2)
+let g2= String(b2)
+let h2= String(c2)
+let i2= String(d2)
+let j2= String(e2)
+console.log(f2)
+console.log(g2)
+console.log(h2)
+console.log(i2)
+console.log(j2)
+

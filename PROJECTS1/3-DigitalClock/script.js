a645e05dc6a068508f11291f5b9131190acb9a4c
Aksh Gupta <240108006@hbtu.ac.in>
2026-06-20 19:06:32 +0530

    Delete PROJECTS1 directory

--- PROJECTS1/3-DigitalClock/script.js
+++
@@ -1,8 +0,0 @@
-
-let result = document.querySelector(".result")
-result.addEventListener("click",function(e){
-    setInterval(function(){
-        let date = new Date()
-        result.innerHTML=date.toLocaleTimeString()
-    },1000)
-})
